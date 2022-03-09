d4421f5617b489fa256c743a3c0c8e2bea46ef51
Mohamed Salah <99357937+mosalahshawky@users.noreply.github.com>
2022-03-09 16:40:56 +0200

    Add files via upload

---
+++ bellabeat Case study.R
@@ -0,0 +1,55 @@
+#01- load required libraries
+library(tidyverse)
+library(lubridate)
+
+#02- import all the CSV Files into R
+calories_Per_hour <- read.csv("D:/Case Studies/track 2/Fitabase Data/DataSets/calories_Per_hour.csv")
+intensities_per_hour <- read.csv("D:/Case Studies/track 2/Fitabase Data/DataSets/intensities_per_hour.csv")
+Sleep_records <- read.csv("D:/Case Studies/track 2/Fitabase Data/DataSets/Sleep_records.csv")
+steps_per_hour <- read.csv("D:/Case Studies/track 2/Fitabase Data/DataSets/steps_per_hour.csv")
+
+
+
+#03- change hours and days to be date and time in all the tables
+calories_Per_hour$Hour <- as.factor(calories_Per_hour$Hour)
+calories_Per_hour$Hour <- as.POSIXct(calories_Per_hour$Hour, format= "%m/%d/%Y %H:%M")
+calories_Per_hour$Day <- as.factor(calories_Per_hour$Day)
+calories_Per_hour$Day <- as.POSIXct(calories_Per_hour$Day, format= "%d/%m/%Y")
+
+intensities_per_hour$Hour <- as.factor(intensities_per_hour$Hour)
+intensities_per_hour$Hour <- as.POSIXct(intensities_per_hour$Hour, format= "%m/%d/%Y %H:%M")
+intensities_per_hour$Day <- as.factor(intensities_per_hour$Day)
+intensities_per_hour$Day <- as.POSIXct(intensities_per_hour$Day, format= "%d/%m/%Y")
+
+Sleep_records$Day <- as.factor(Sleep_records$Day)
+Sleep_records$Day <- as.POSIXct(Sleep_records$Day, format= "%d/%m/%Y")
+
+steps_per_hour$Hour <- as.factor(steps_per_hour$Hour)
+steps_per_hour$Hour <- as.POSIXct(steps_per_hour$Hour, format= "%m/%d/%Y %H:%M")
+steps_per_hour$Day <- as.factor(steps_per_hour$Day)
+steps_per_hour$Day <- as.POSIXct(steps_per_hour$Day, format= "%d/%m/%Y")
+
+#04- Join calories_Per_hour, intensities_per_hour & steps_per_hour
+
+data_per_hour1 <- merge(calories_Per_hour, steps_per_hour)
+data_per_hour <- merge(data_per_hour1, intensities_per_hour)
+
+
+
+#05- add week_day to the files
+data_per_hour$day_of_week <- format(as.Date(data_per_hour$Day), "%A")
+Sleep_records$day_of_week <- format(as.Date(Sleep_records$Day), "%A")
+
+
+#07- Export the edited CSV files to be prepare for the visualization step
+
+write.csv(data_per_hour, file ="D:/Case Studies/track 2/Fitabase Data/DataSets/Final Data sets/data_per_hour.csv")
+write.csv(Sleep_records, file ="D:/Case Studies/track 2/Fitabase Data/DataSets/Final Data sets/Sleep_records.csv")
+
+
+
+
+glimpse(Sleep_records) #will show average sleeping hours per day of week
+glimpse(data_per_hour) #will show relations between (calories & Steps) 
+
+
